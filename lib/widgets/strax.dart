fb6d96d70c2e17216c6f0b7eda2246681ca66099
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-19 18:14:58 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -30,3 +30,4 @@ import 'package:pangolin/utils/providers/customization_provider.dart';
 import 'package:pangolin/utils/providers/icon_provider.dart';
 import 'package:pangolin/utils/providers/io_provider.dart';
 import 'package:pangolin/utils/providers/misc_provider.dart';
+import 'package:pangolin/utils/providers/search_provider.dart';
