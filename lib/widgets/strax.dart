0318c8d7b638b62344855f038e93ba4235e9a789
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-19 18:13:17 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -9,3 +9,4 @@ You may obtain a copy of the License at
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
