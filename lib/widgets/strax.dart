e8f217f147cf03cc64cf37b16319aea7cb4c1cf7
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-20 15:34:50 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -79,6 +79,3 @@ Future<void> main() async {
             value: MiscProvider(),
           ),
           ChangeNotifierProvider<ClockProvider>.value(
-            value: ClockProvider(),
-          ),
-
