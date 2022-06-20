5d557f2f4520af2612fc4257881fe18c19664a7d
Rwema <52289151+rwema3@users.noreply.github.com>
2022-06-20 15:35:35 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -98,6 +98,7 @@ class Pangolin extends StatelessWidget {
     return MaterialApp(
       home: const Desktop(),
       theme: theme(context),
+      locale: context.locale,
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
     );
