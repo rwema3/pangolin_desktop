6c644feaa3c763b2f547729fb64b692cde53846e
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-03 22:37:04 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -48,7 +48,9 @@ Future<void> main() async {
   await EasyLocalization.ensureInitialized();
 
   //load visual engine
-  
+  await loadVisualEngine();
+  if (kIsWeb == false) {
+    
           ChangeNotifierProvider<ClockProvider>.value(
             value: ClockProvider(),
           ),
