cce1b4eb0a38403ca3f5725371aa92e82f2658af
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-04 17:15:51 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -34,10 +34,6 @@ import 'package:pangolin/utils/providers/search_provider.dart';
 import 'package:pangolin/utils/theme/theme.dart';
 import 'package:provider/provider.dart';
 
-          ),
-          ChangeNotifierProvider<CustomizationProvider>.value(
-            value: CustomizationProvider(),
-          ),
           ChangeNotifierProvider<MiscProvider>.value(
             value: MiscProvider(),
           ),
