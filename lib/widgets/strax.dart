c813752c2d76ccaa8bb95f7ed5c00745ca8d4486
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-04 17:16:28 +0200

    //initialize scheduler for time and date   DateTimeManager.initialiseScheduler();

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -34,6 +34,13 @@ import 'package:pangolin/utils/providers/search_provider.dart';
 import 'package:pangolin/utils/theme/theme.dart';
 import 'package:provider/provider.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  
+  //initialize scheduler for time and date
+  DateTimeManager.initialiseScheduler();
+
   //initialize the localization engine
   await EasyLocalization.ensureInitialized();
 
