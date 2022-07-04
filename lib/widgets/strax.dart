b66e34f960ae72fa5a5a664c707d24492eb11daf
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-04 17:16:12 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -34,6 +34,12 @@ import 'package:pangolin/utils/providers/search_provider.dart';
 import 'package:pangolin/utils/theme/theme.dart';
 import 'package:provider/provider.dart';
 
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: Locales.supported,
+      fallbackLocale: const Locale("en", "US"),
+      useFallbackTranslations: false,
       assetLoader: GeneratedAssetLoader(),
       path: "assets/locales",
       startLocale: const Locale("en", "US"),
