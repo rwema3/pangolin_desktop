cae8110a3a48df91a518fbebc82123f0f948d334
Rwema <52289151+rwema3@users.noreply.github.com>
2022-07-09 18:31:16 +0200

    Update strax.dart

--- lib/widgets/strax.dart
+++ lib/widgets/strax.dart
@@ -92,8 +92,6 @@ import 'package:provider/provider.dart';
   );
 }
 
-class Pangolin extends StatelessWidget {
-  const Pangolin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
